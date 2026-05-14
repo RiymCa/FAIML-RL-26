6d5f687785ab1e271e85338130a2bab60c8dc8a9
PietroFabi <pietrofabi10@gmail.com>
2026-05-14 15:53:20 +0200

    Update train_sb3.py
    Added model initializaton and training with PPO, different options for arg parse, added environment to be rand_wrapped.
    Missing handling of the adr, missing comparison with SAC algorithm, missing saving of the models.

--- part2/train_sb3.py
+++ part2/train_sb3.py
@@ -3,8 +3,9 @@ from collections import deque
 
 import gymnasium as gym
 import numpy as np
+import time
 import panda_gym  # type: ignore[import-not-found]
-from stable_baselines3 import DDPG
+from stable_baselines3 import PPO, SAC
 from rand_wrapper import RandomizationWrapper
 
 
@@ -27,26 +28,38 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--timesteps",
         type=int,
-        default=500_000,
+        default=1000000,
+        choices=[10000, 100000, 500000, 1000000],
+        # 1.000 -> 40 s
+        # 1.000.000 ->
         help="Number of training timesteps",
     )
     return parser.parse_args()
 
 
 def main() -> None:
+    time_start = time.time()
     args = parse_args()
+    render = False
 
     env = gym.make(
         "PandaPush-v3",
-        render_mode="rgb_array",
+        render_mode="human" if render else "rgb_array",
         type=args.env_type,
         reward_type="dense",
     )
+    env = RandomizationWrapper(env, mode=args.sampling_strategy)
 
-    #TODO: add randomization wrapper here
-    #TODO: create model and train it
+    model = PPO("MultiInputPolicy", env, verbose=0)
+    model.learn(total_timesteps=args.timesteps)
+
+    time_end = time.time()
+    print(f"time passed -> {int((time_end - time_start)/3600)}:{int((time_end - time_start)/60)}:{(time_end - time_start) % 60:.2f}")
+
     save_name = f"sac_push_{args.sampling_strategy}_{args.env_type}_{args.timesteps // 1000}k"
     # TODO: model.save(save_name)
+
+    env.close()
 
 
 if __name__ == "__main__":
